768cfddc521a2807a8604dc3de00f472867d2152
Егор <egor_nefedov.2002@mail.ru>
2022-11-16 19:20:42 +0300

    Круг и треугольник

---
+++ FigureSquare/Triangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureSquare
+{
+    internal class Triangle
+    {
+        double first { get; set; }
+        double second { get; set; }
+        double third { get; set; }
+        public Triangle(double _first, double _second, double _third)
+        {
+            first = _first;
+            second = _second;
+            third = _third;
+        }
+
+        public double TriangleSquare()
+        {
+            var SemiPerimetr = (first + second + third) / 2;
+            var Square = Math.Round(Math.Sqrt(SemiPerimetr*(SemiPerimetr-first) *(SemiPerimetr-second) *(SemiPerimetr-third)), 2);
+            return Square;
+        }
+    }
+}
